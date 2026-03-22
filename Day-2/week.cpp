e65b878af3ae8b271e28f050c705703624e462bb
Arun Sah <saharun2056@gmail.com>
2026-03-22 15:33:01 +0530

    learn about vowel, consnt, and display the day

---
+++ Day-2/week.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a nember Day between 1 to 7: ";
+    cin>>num;
+    if (num == 1)
+    {
+        cout<<"Monday";
+    }
+    else if (num == 2)
+    {
+        cout<<"Tuesday";
+    }
+    else if (num == 3)
+    {
+        cout<<"Wednesday";
+    }
+    else if (num == 4)
+    {
+        cout<<"Thursday";
+    }
+    else if (num == 5)
+    {
+        cout<<"Friday";
+    }
+    else if (num == 6)
+    {
+        cout<<"Saturday";
+    }
+    else if (num == 7)
+    {
+        cout<<"Sunday";
+    }
+    else
+    {
+        cout<<"Invalid input";
+    }
+}
