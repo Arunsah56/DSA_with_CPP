7e534beb41d2fa003e556e4a894db3813f9bd96f
Arun Sah <saharun2056@gmail.com>
2026-03-28 01:10:47 +0530

    tri pattern update

--- Day-4/tri_patten.cpp
+++ Day-4/tri_patten.cpp
@@ -84,4 +84,118 @@ int main()
         }
         cout<<endl;
     }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if( j >= i)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+            
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            if(i == j)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            if( j >= i)
+            {
+                cout<<i<<" ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+            
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int row=1; row<=n; row++)
+    {
+        for(int col=1; col<=n-row; col++)
+        cout<<" ";
+
+        for(int col=1; col<=row; col++)
+        cout<<col<<" ";
+
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int row=1; row<=n; row++)
+    {
+        for(int col=1; col<=n-row; col++)
+        cout<<"  ";
+
+        for(int col=1; col<=row; col++)
+        cout<<col<<" ";
+
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int row=1; row<=n; row++)
+    {
+        char ch = 'A';
+        for(int col=1; col<=n-row; col++)
+        cout<<"  ";
+
+        for(int col=1; col<=row; col++)
+        {
+            cout<<ch<<" ";
+            ch++;
+        }
+        
+
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for(int row=n; row>=1; row--)
+    {
+        for(int col=1; col<=row; col++)
+        cout<<col<<" ";
+        for(int col=1; col<=n-row; col++)
+        cout<<"  ";
+
+        
+
+        cout<<endl;
+    }
 }
