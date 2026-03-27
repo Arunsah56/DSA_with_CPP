f4d3c813f305ac1960538f23bce35b505bdeb894
Arun Sah <saharun2056@gmail.com>
2026-03-27 23:40:03 +0530

    learn to print the pattern with various method

--- Day-4/sq_patten.cpp
+++ Day-4/sq_patten.cpp
@@ -14,4 +14,73 @@ int main()
         }
         cout<<endl;
     }
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for( int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for( int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for( int i=1; i<=n; i++)
+    {
+        for(int j=n; j>=1; j--)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    for( int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout<<j*j<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    char ch = 'a';
+    for( int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+        ch = ch + 1;
+    }
+
+    cout<<endl;
+    cout<<"Number pattern: "<<endl;
+    int count = 1;
+    for( int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n; j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
 }
